bdcc07eaeb917bb3431b762012aef4b08c2f8c17
andredev <andresebastian056@gmail.com>
2026-04-22 21:43:28 -0300

    estilizações das primeiras seções da página home

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,18 +1,66 @@
 import "./home.css"
 
+import { Link } from "react-router-dom";
 
 
  function Home(){
     return(
         <div className="container">
-            <div className="main">
+            <main className="main">
                 <div className="descricao-main">
                     <h1>Pé de Pipa</h1>
-                    <span>Levando água para o nosso povo</span>
+                    <span>Mais que água. Tranquilidade para o seu dia a dia.</span>
+                    <Link to="/pedido">Faça seu Pedido</Link>
                 </div>
+            </main>
+            
+            <section className="descricao-secao">
+                <h2>Bem Vindo!</h2>
+                <p>
+                    Em lugares onde a água não chega com facilidade, a gente faz questão de chegar.
+                    Somos especialistas na distribuição de água potável com caminhões-pipa,
+                    levando mais do que abastecimento: levamos tranquilidade, dignidade e continuidade para o dia a dia.
+                </p>
+                <p>
+                    Com uma operação ágil e compromisso com a qualidade, atendemos residências, comércios, propriedades
+                    rurais e obras, sempre com responsabilidade e confiança.
+                </p>
+                <p>
+                    Porque água não é luxo, é essencial. E garantir esse acesso é o que nos move.
+                </p>
+            </section>
 
-            </div>
-            <h1>Página home</h1>
+            <section className="descricao-secao-2">
+                <h2>Como Pedir?</h2>
+                <p>
+                    para pedir lorem blabla bla jotombe flueque tiforme jofundio to goia tuisol fuledimarto xo dlica i gunai
+                    goia tuisol fuledimarto xo dlica i gunai tiforme jofundio to goia tuisol fuledimarto xo goia tuisol 
+                    bla jotombe flueque tiforme jofundio to goia tuisol fuledimarto.
+                </p>
+                <p>
+                    jotombe flueque tiforme jofundio to goia tuisol fuledimarto xo dlica i gunai
+                    goia tuisol fuledimarto xo dlica i gunai tiforme jofundio to goia tuisol fuledimarto xo goia tuisol 
+                    bla jotombe flueque tiforme jofundio to goia tuisol fuledimarto.
+                </p>
+            </section>
+
+            <section className="descricao-secao-2">
+                <h2>
+                    Serviços
+                </h2>
+                <p>
+                     Sobre os nossos serviços lorem blabla bla jotombe flueque tiforme jofundio to goia tuisol fuledimarto xo dlica
+                    i gunai goia tuisol fuledimarto xo dlica i gunai tiforme jofundio to goia tuisol fuledimarto xo goia tuisol 
+                    bla jotombe flueque tiforme jofundio to goia tuisol fuledimarto.
+                </p>
+                <p>
+                    tiforme jofundio to goia tuisol fuledimarto xo goia tuisol 
+                    bla jotombe flueque tiforme jofundio to goia tuisol fuledimarto flueque tiforme jofundio to goia tuisol fuledi
+                    marto xo dlica i gunai, goia tuisol fuledimarto xo dlica i gunai tiforme.
+                </p>
+            </section>
+            
+            
             
         </div>
     )
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,11 @@
 import "./footer.css"
 
+
+// Conectar o footer com o meu Linkedin e Git hub.
 function Footer(){
     return(
         <footer className="footer">
-            <h1>Texte</h1>
+            <h3><a href="www.linkedin.com/in/andré-sebastian-2293393b2">Linkedin</a></h3>
         </footer>
     )
 }
